642f4ae0b015f275eb9ee2f2e83854594ca729f4
cdJow <juvinpelimer01@gmail.com>
2024-12-30 02:10:54 +0800

    Dec 30 1st Commit

---
+++ src/views/pages/Rates/Testfile.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+
+<template>
+    <div class="card">
+        <h1>test</h1>
+    </div>
+</template>
+
+<style scoped lang="scss">
+:deep(.p-datatable-frozen-tbody) {
+    font-weight: bold;
+}
+
+:deep(.p-datatable-scrollable .p-frozen-column) {
+    font-weight: bold;
+}
+</style>
